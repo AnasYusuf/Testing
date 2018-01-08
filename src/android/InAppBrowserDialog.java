0f125c25bbe498cbab0594fed709d72e87f5af7e
AnasYusuf <anasyusuf1@yahoo.com>
2018-01-09 00:40:48 +0800

    updated

--- src/android/InAppBrowserDialog.java
+++ src/android/InAppBrowserDialog.java
@@ -33,6 +33,8 @@ public class InAppBrowserDialog extends Dialog {
     Context context;
     InAppBrowser inAppBrowser = null;
 
+    public InAppBrowserDialog();
+
     public InAppBrowserDialog(Context context, int theme) {
         super(context, theme);
         this.context = context;
